11537ab5b7eede9fc7d9e7d1dcb6a6c673c00329
Avish <avishbhatt@gmail.com>
2021-01-21 14:29:52 -0500

    Update tkinterGui.py
    updates/new feature added

--- tkinterGui.py
+++ tkinterGui.py
@@ -1,32 +1,39 @@
 import tkinter as tk
 from PIL import ImageTk, Image
+from subprocess import call
 
 window = tk.Tk()
 window.title("Cycle Safe")
+window.geometry("600x600")
+prox_colour = "green"#default state
+prox_state = "All Clear!"#default state
 
 #create a top frame and lower frame
-frame = tk.Frame(window,width=300,height=300,bg="tomato")
-frame.pack()
-lower_frame = tk.Frame(window,width=300,height=100,bg="black")
+top_frame = tk.Frame(window,width=600,height=600,bg="green")#could change frame colour based on proximity
+top_frame.pack()
+mid_frame = tk.Frame(window, width=600, height=600, bg=prox_colour)#proximity determines colour
+mid_frame.pack()
+lower_frame = tk.Frame(window,width=600,height=100,bg="black")
 lower_frame.pack()
 
 #create text label on frame and add if statment to display different messages by changing labelText var
-labelText = "All Clear!" #white background is an issue !!!
-text1 = tk.Label(frame,text=labelText)
+labelText = prox_state 
+text1 = tk.Label(top_frame,text=labelText, background=prox_colour)#can change background colour based on proximity
 text1.place(x=0,y=0)
 text1.pack()
 
 #import photos from direcotry and add if statment to upload different images 
 #render = ImageTk.PhotoImage(Image.open("foo.png"))
 #render = ImageTk.PhotoImage(Image.open("foo1.png"))
-render = ImageTk.PhotoImage(Image.open("foo2.png"))
-img = tk.Label(frame,image=render)
-img.image = render
-img.place(x=0,y=0)
-img.pack()
+##render = ImageTk.PhotoImage(Image.open("foo2.png"))
+##img = tk.Label(frame,image=render)
+##img.image = render
+##img.place(x=0,y=0)
+##img.pack()
 
 #create widgets for lower frame
-btn_shutdown = tk.Button(lower_frame,text="shutdown")
+btn_shutdown = tk.Button(lower_frame,text="shutdown", background="teal")
+##btn_shutdown = tk.Button(lower_frame,text="shutdown", command = shutDown)#shutdown preocedure for Pi
 text2 = tk.Label(lower_frame,text="lidarSensor")
 text3 = tk.Label(lower_frame,text="SystemSettings")
 
@@ -34,6 +41,10 @@ text3 = tk.Label(lower_frame,text="SystemSettings")
 btn_shutdown.grid(row=0,column=0)
 text2.grid(row=0,column=1)
 text3.grid(row=0,column=2)
+
+#Shutdown function
+##def shutDown():
+##    call("sudo shutdown -h now", shell=True)
 
 
 window.mainloop()
