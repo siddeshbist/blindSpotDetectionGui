451d0e4620a3203ba04190c47fd20d4291ad1052
Avish <avishbhatt@gmail.com>
2021-02-20 18:40:26 -0500

    Update ultrasonic_multithread.py

--- ultrasonic_multithread.py
+++ ultrasonic_multithread.py
@@ -4,24 +4,28 @@ import threading
 
 distance =0;
 
-def ultrasonic ():
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(echoPIN,GPIO.IN)
-    GPIO.setup(triggerPIN,GPIO.OUT)
-    GPIO.setwarnings(True)
- while True:
-     echoPIN = 15
-     triggerPIN = 14
+echoPIN = 15
+triggerPIN = 14
+     
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(echoPIN,GPIO.IN)
+GPIO.setup(triggerPIN,GPIO.OUT)
+GPIO.setwarnings(True)
 
-     global distance
+def ultrasonic ():
+ while(1):
+     global distance = 0
      duration = 0
+
+     #send trigger
      GPIO.output(triggerPIN, 0)
      time.sleep(0.000002)
      GPIO.output(triggerPIN, 1)
      time.sleep(0.000010)
      GPIO.output(triggerPIN, 0)
      time.sleep(0.000002)
+     
      # wait for echo reading
      while GPIO.input(echoPIN) == 0: pass
      startT = time.time()
@@ -39,7 +43,6 @@ def ultrasonic ():
      time.sleep(0.2)
  
 def main():
-    global distance
     x=threading.Thread(target=ultrasonic)
     x.start()
     while(1):
